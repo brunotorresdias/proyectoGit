ab6951657d850d1f794116df71065b9ded1aad61
brunotorresdias <torresdiasbruno11@gmail.com>
2023-08-07 17:05:39 -0300

    Initial commit

---
+++ src/proyectogit/ProyectoGit.java
@@ -0,0 +1,17 @@
+
+package proyectogit;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProyectoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        cuenta c1 = new cuenta("bruno_torres", "cont1234");
+    }
+    
+}
